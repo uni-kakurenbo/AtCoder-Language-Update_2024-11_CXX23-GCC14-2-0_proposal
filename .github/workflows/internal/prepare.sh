8362ec00c9fb20f9cb78548940ab6ec83e031df3
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-01-26 16:16:02 +0900

    pkg-config

---
+++ .github/workflows/internal/prepare.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+WORKING_DIRECTORY="$(dirname "$0")"
+
+echo "::group::dasel"
+dasel --version || sudo apt-get install -y dasel
+echo "::endgroup::"
+
+echo "::group::jq"
+jq --version || sudo apt-get install -y jq
+echo "::endgroup::"
+
+echo "::group::pkg-config"
+"${WORKING_DIRECTORY}/tools/pkg-config.sh"
+echo "::endgroup::"
+
+echo "::group::taplo"
+"${WORKING_DIRECTORY}/tools/taplo.sh"
+echo "::endgroup::"
+
+rm -rf ./dist/ && mkdir -p ./dist/
