105e70f9e5641632c521717a73b12554926ccf04
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-02-12 18:51:54 +0900

    fixup! variant for clang

--- .github/workflows/internal/replacers/pkg-config.sh
+++ .github/workflows/internal/replacers/pkg-config.sh
@@ -14,7 +14,7 @@ function replace() {
     dasel -r yaml -w json <"${target}" >"${DIST_DIR}/.tmp.json"
 
     jq -r --arg variant "$(basename "${DIST_DIR}")" --slurpfile this "${DIST_DIR}/.tmp.json" \
-        '.Cflags += "" | .Libs += "" | with_entries(.value += $this[0][$variant][.key])' "${DIST_DIR}/.tmp.json" |
+        '.Cflags += "" | .Libs += "" | with_entries(.value += $this[0].variant[$variant][.key]) | del(.variant)' "${DIST_DIR}/.tmp.json" |
         dasel -r json -w yaml |
         sed -E -e 's/\\//g' -e 's/ +/ /g' >"${dist}"
 }
